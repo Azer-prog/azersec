dbf046cc9c1f75f3aa060a5e028e7046f4498818
Azer-prog <azer.sebei@esprit.tn>
2022-10-29 16:47:33 -0700

    azerlive

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221029.163815';
+const cacheName = 'chirpy-20221029.164609';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
